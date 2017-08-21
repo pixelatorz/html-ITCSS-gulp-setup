3a22596c1584673b66ed5a70895ba067c28d7a14
Kris Van Herzeele <kris.vanherzeele@gmail.com>
2017-08-21 12:55:14 +0200

    setup

---
+++ README.md
@@ -0,0 +1,8 @@
+# simple html - sass setup
+
+starting grunt:
+```
+> node_modules/.bin/grunt
+```
+
+go to : http://localhost:8080/
---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+  connect = require('gulp-connect');
+
+
+gulp.task('styles', function() {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('html', function () {
+  gulp.src('./app/*.html')
+    .pipe(connect.reload());
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: '.',
+    livereload: true
+  });
+});
+
+//Watch task
+gulp.task('watch',function() {
+    gulp.watch('sass/**/*.scss',['styles']);
+    gulp.watch('sass/**/*.html',['styles']);
+
+});
+
+//Watch task
+gulp.task('default', ['connect', 'watch']);
